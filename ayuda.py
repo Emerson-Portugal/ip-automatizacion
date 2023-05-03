dfefd97ac97f68bc811acc5c1faae82b1922a8a2
Emerson-Portugal <emersoncrp@gmail.com>
2023-05-03 02:36:00 -0500

    IP_p

---
+++ ayuda.py
@@ -0,0 +1,4 @@
+cadena = "Hola.mundo"
+separador = "."
+separado_por_espacios = cadena.split(separador)
+print("Separado por espacios es:", separado_por_espacios)
--- ip_redes.py
+++ ip_redes.py
@@ -1,2 +1,30 @@
-ip = print(input("Cual es tu IP: "))
-print(ip)
+
+
+def solicitar_ip():
+    ip = input("Cual es tu IP: ")
+    clase_red(ip)
+
+
+def clase_red(ip):
+    separador = "."
+    separado = ip.split(separador)
+    
+    if int(separado[0]) >= 0 and int(separado[0]) <= 127:
+        print("Clase A")
+    elif int(separado[0]) >= 128 and int(separado[0]) <= 191:
+        print("Clase B")
+    elif int(separado[0]) >= 192 and int(separado[0]) <= 223:
+        print("Clase C")
+    else :
+        print("Ningun Clase")
+
+
+
+def main():
+    """Función principal"""
+    solicitar_ip()
+
+
+
+if __name__ == "__main__":
+    main()
