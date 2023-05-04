2faaeef7c5130dfc8ce45340460e4203d1419aba
Emerson-Portugal <emersoncrp@gmail.com>
2023-05-04 06:50:42 -0500

    Conversion a binario

--- ayuda.py
+++ ayuda.py
@@ -1,4 +1,18 @@
-cadena = "Hola.mundo"
-separador = "."
-separado_por_espacios = cadena.split(separador)
-print("Separado por espacios es:", separado_por_espacios)
+ip = '111.21.21.21'
+
+# Divide la dirección IP en sus cuatro componentes
+componentes = ip.split('.')
+
+# Convierte cada componente en un entero
+componente1 = int(componentes[0])
+componente2 = int(componentes[1])
+componente3 = int(componentes[2])
+componente4 = int(componentes[3])
+
+# Combina los componentes en un solo valor numérico
+
+componente1 = (bin(componente1)[2:])
+componente2 = (bin(componente2)[2:])
+componente3 = (bin(componente3)[2:])
+componente4 = (bin(componente4)[2:])
+
--- ip_redes.py
+++ ip_redes.py
@@ -1,3 +1,24 @@
+def conversion_bin(x):
+    ip = '111.21.21.21'
+
+    # Divide la dirección IP en sus cuatro componentes
+    componentes = ip.split('.')
+
+    # Convierte cada componente en un entero
+    componente1 = int(componentes[0])
+    componente2 = int(componentes[1])
+    componente3 = int(componentes[2])
+    componente4 = int(componentes[3])
+
+    # Combina los componentes en un solo valor numérico
+    valor_numerico = (componente1 << 24) + (componente2 << 16) + (componente3 << 8) + componente4
+
+    # Convierte el valor numérico a flotante
+    nueva_ip = float(valor_numerico)
+
+    print(nueva_ip)
+
+
 
 
 def solicitar_ip():
@@ -8,17 +29,33 @@ def solicitar_ip():
 def clase_red(ip):
     separador = "."
     separado = ip.split(separador)
+
     
     if int(separado[0]) >= 0 and int(separado[0]) <= 127:
+        mascara = "255.0.0.0"
         print("Clase A")
+        direccion_red(ip, mascara)
+        
+
     elif int(separado[0]) >= 128 and int(separado[0]) <= 191:
+        mascara = "255.255.0.0"
         print("Clase B")
+        direccion_red(ip, mascara)
+
     elif int(separado[0]) >= 192 and int(separado[0]) <= 223:
+        mascara = "255.255.255.0"
         print("Clase C")
+        direccion_red(ip, mascara)
+
     else :
         print("Ningun Clase")
 
 
+
+def direccion_red(ip,mascara):
+    print(ip + " " + mascara )
+    nueva_ip = conversion_bin(ip)
+    nueva_mascara = conversion_bin(mascara)
 
 def main():
     """Función principal"""
