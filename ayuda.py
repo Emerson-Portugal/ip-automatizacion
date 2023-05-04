7fcd52f9a29f57e60421e824131570b9d188fba0
Emerson-Portugal <emersoncrp@gmail.com>
2023-05-04 06:52:20 -0500

    Conversion a binario-2

--- ayuda.py
+++ ayuda.py
@@ -16,3 +16,21 @@ componente2 = (bin(componente2)[2:])
 componente3 = (bin(componente3)[2:])
 componente4 = (bin(componente4)[2:])
 
+
+""" 
+# Defina dos números enteros
+a = 10   # Representación binaria: 0b1010
+b = 6    # Representación binaria: 0b0110
+
+# Realice la operación AND a nivel de bit entre los dos números
+c = a & b
+
+# Imprima los números y su resultado en su representación binaria y entera
+print("a en binario:", bin(a))
+print("b en binario:", bin(b))
+print("c en binario:", bin(c))
+print("a en entero:", a)
+print("b en entero:", b)
+print("c en entero:", c)
+"""
+
