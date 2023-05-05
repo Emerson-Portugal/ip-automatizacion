5af69c1a17e04983041a83b733935e052b5e1ec8
Emerson-Portugal <emersoncrp@gmail.com>
2023-05-04 19:48:30 -0500

    Mejoras direccion_red

--- ip_redes.py
+++ ip_redes.py
@@ -1,28 +1,6 @@
-def conversion_bin(x):
-    ip = '111.21.21.21'
-
-    # Divide la dirección IP en sus cuatro componentes
-    componentes = ip.split('.')
-
-    # Convierte cada componente en un entero
-    componente1 = int(componentes[0])
-    componente2 = int(componentes[1])
-    componente3 = int(componentes[2])
-    componente4 = int(componentes[3])
-
-    # Combina los componentes en un solo valor numérico
-    valor_numerico = (componente1 << 24) + (componente2 << 16) + (componente3 << 8) + componente4
-
-    # Convierte el valor numérico a flotante
-    nueva_ip = float(valor_numerico)
-
-    print(nueva_ip)
-
-
-
-
 def solicitar_ip():
-    ip = input("Cual es tu IP: ")
+    ip = "192.168.55.44"
+    # ip = input("Cual es tu IP: ")
     clase_red(ip)
 
 
@@ -30,12 +8,10 @@ def clase_red(ip):
     separador = "."
     separado = ip.split(separador)
 
-    
     if int(separado[0]) >= 0 and int(separado[0]) <= 127:
         mascara = "255.0.0.0"
         print("Clase A")
         direccion_red(ip, mascara)
-        
 
     elif int(separado[0]) >= 128 and int(separado[0]) <= 191:
         mascara = "255.255.0.0"
@@ -47,20 +23,28 @@ def clase_red(ip):
         print("Clase C")
         direccion_red(ip, mascara)
 
-    else :
+    else:
         print("Ningun Clase")
 
 
+def direccion_red(ip, mascara):
+    separador = "."
+    separado_ip = ip.split(separador)
+    separado_mascara = mascara.split(separador)
 
-def direccion_red(ip,mascara):
-    print(ip + " " + mascara )
-    nueva_ip = conversion_bin(ip)
-    nueva_mascara = conversion_bin(mascara)
+    valor_nuevo = []
+
+    for i in range(len(separado_ip)):
+        valor_nuevo.append(str(int(separado_ip[i]) & int(separado_mascara[i])))
+
+    print(valor_nuevo)
+    # direccion_red = ".".join(valor_nuevo)
+    # return direccion_red
+
 
 def main():
     """Función principal"""
     solicitar_ip()
-
 
 
 if __name__ == "__main__":
